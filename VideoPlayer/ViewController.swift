b48c9bd77f1db2b1f2ca544da71816729b315190
Mahyar <mahyar.sh021@yahoo.com>
2022-11-22 13:50:44 +0330

    Initial Commit

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by MahyR Sh on 11/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
